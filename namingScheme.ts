e5e25983875fecc4f09cefcfb068ea7966272090
Pete <kungfusheep@gmail.com>
2015-10-08 09:11:03 +0100

    initial

---
+++ namingScheme.ts
@@ -0,0 +1,21 @@
+/// <reference path="node_modules/tslint/typings/typescriptServices.d.ts" />
+/// <reference path="node_modules/tslint/lib/tslint.d.ts" />
+
+
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "import statement forbidden";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoImportsWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoImportsWalker extends Lint.RuleWalker {
+    public visitImportDeclaration(node: ts.ImportDeclaration) {
+        // create a failure at the current position
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+
+        // call the base version of this visitor to actually parse this node
+        super.visitImportDeclaration(node);
+    }
+}
